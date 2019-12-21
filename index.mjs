4bcb38dedcbe56dd0283831307d51bc02c9b2203
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-12-21 14:42:06 -0800

    Implement getEnergy

--- index.mjs
+++ index.mjs
@@ -4,7 +4,9 @@ import SimulatedAnnealing from "simulated-annealing";
 
 import chunk from "chunk";
 
-import Combinatorics from "js-combinatorics"
+import Combinatorics from "js-combinatorics";
+
+import sum from "math-sum";
 
 import d3 from "d3-array";
 import uniqueRandomArray from "unique-random-array";
@@ -15,8 +17,9 @@ import randomArrayIndex from "random-array-index";
 const TEMP_MAX = 15;
 const TEMP_MIN = 0.001;
 const COOLING_RATE = 0.001;
+const ALPHA = (BETA = 1);
 
-const UniqueGrouping = (people, groupSize) => {
+const UniqueGrouping = (people, groupSize, sessions = 0) => {
   // const array_chunks = (array, chunk_size) =>
   //   Array(Math.ceil(array.length / chunk_size))
   //     .fill()
@@ -30,12 +33,35 @@ const UniqueGrouping = (people, groupSize) => {
   // console.log(groups);
 
   const getEnergy = v => {
-    v.forEach(group => {
-      const cmb = Combinatorics.combination(group, 2);
-      cmb.map(pair => {
-        
-      })
-    })
+    v.map(group => {
+      const costFunction = group => {
+        const cmb = Combinatorics.combination(group, 2);
+        return (
+          sum(
+            cmb.map(pair => {
+              const decayFunction = (student1, student2) => {
+                const {
+                  timeSinceLastSeen,
+                  timesSeen
+                } = student1.blacklist.find(
+                  student => student.id === student2.id
+                );
+
+                return (
+                  Math.exp(timeSinceLastSeen / (sessions + 1)) * (timesSeen + 1)
+                );
+              };
+
+              const [student1, student2] = pair;
+              return ALPHA * decayFunction(student1, student2);
+            })
+          ) +
+          BETA * group.length
+        );
+      };
+
+      return costFunction(group);
+    });
   };
 
   const newState = x => {
@@ -54,7 +80,7 @@ const UniqueGrouping = (people, groupSize) => {
 
     const randomGroup = uniqueRandomArray(x);
 
-    // randomly either swaps two random students from two different groups 
+    // randomly either swaps two random students from two different groups
     // or moves one random student to another group
     if (random.boolean()) {
       swapStudents(randomGroup(), randomGroup());
--- quokka.js
+++ quokka.js
@@ -1,3 +1,4 @@
+var chunk = require("chunk");
 var simulatedAnnealing = require("simulated-annealing");
 const { performance } = require("perf_hooks");
 const { boolean } = require("random");
@@ -34,3 +35,38 @@ console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
 
 console.log(`result: ${result}`);
 result;
+const ALPHA = (BETA = 1);
+
+ALPHA;
+BETA;
+console.log(chunk([1, 2, 3, 4, 5], 2));
+
+const test1 = {
+  prop1: "oof",
+  prop2: 6
+};
+
+const test2 = {
+  prop1: "oof",
+  prop2: 6
+};
+
+const test = [
+  {
+    prop1: "fake",
+    prop2: 10
+  },
+  {
+    prop1: "fake",
+    prop2: 10
+  },
+  test1
+];
+
+console.log(test.find(obj => obj === test2))
+console.log(test.find(obj => obj === test1))
+
+console.log(Object.is(test1, test2))
+
+
+console.log(JSON.stringify(test1) === JSON.stringify(test2))
